2bf97eedbd9e334c6fc71c608ac146680e6f6696
Juanfran Correas <jfcorreas@gmail.com>
2017-06-03 23:37:33 +0200

    Added Angular Router and empty Model-driven-form view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,36 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { ModelDrivenFormComponent } from './model-driven-form/model-driven-form.component';
+
+const appRoutes: Routes = [
+  { path: 'model-driven', component: ModelDrivenFormComponent }/*,
+  { path: 'hero/:id',      component: HeroDetailComponent },
+  {
+    path: 'heroes',
+    component: HeroListComponent,
+    data: { title: 'Heroes List' }
+  },
+  { path: '',
+    redirectTo: '/heroes',
+    pathMatch: 'full'
+  },
+  { path: '**', component: PageNotFoundComponent }*/
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ModelDrivenFormComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
