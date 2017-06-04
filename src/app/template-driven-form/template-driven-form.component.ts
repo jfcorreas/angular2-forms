14b4ff2d6955155dbd5070b5fbabd626fa7d8d5a
Juanfran Correas <jfcorreas@gmail.com>
2017-06-04 20:43:21 +0200

    Template driven form implemented

--- src/app/template-driven-form/template-driven-form.component.ts
+++ src/app/template-driven-form/template-driven-form.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+import { User } from '../user.interface';
 
 @Component({
-  selector: 'app-template-driven-form',
+  selector: 'app-model-driven-form',
   templateUrl: './template-driven-form.component.html',
   styleUrls: ['./template-driven-form.component.css']
 })
-export class TemplateDrivenFormComponent implements OnInit {
 
-  constructor() { }
+export class TemplateDrivenFormComponent implements OnInit {
+  public user: User;
 
   ngOnInit() {
+    this.user = {
+      name: '',
+        address: {
+         street: '',
+         postcode: '8000'
+      }
+    };
   }
 
+  save(model: User, isValid: boolean) {
+    console.log(model, isValid);
+  }
 }
