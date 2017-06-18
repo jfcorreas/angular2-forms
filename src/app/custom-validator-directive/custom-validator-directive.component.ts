6fcdc3a8a508e4d94534dd42dab1390842d33a63
Juanfran Correas <jfcorreas@gmail.com>
2017-06-18 17:07:12 +0200

    EqualValidatorDirective Implemented

--- src/app/custom-validator-directive/custom-validator-directive.component.ts
+++ src/app/custom-validator-directive/custom-validator-directive.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../user.interface';
 
 @Component({
   selector: 'app-custom-validator-directive',
@@ -9,7 +10,25 @@ export class CustomValidatorDirectiveComponent implements OnInit {
 
   constructor() { }
 
+  public user: User;
+
   ngOnInit() {
+    this.user = {
+      name: '',
+      address: {
+         street: '',
+         postcode: '8000'
+      },      
+      username: '',
+      email: '',
+      password: '',
+      confirmPassword: ''
+    }
+  }
+
+  save(model: User, isValid: boolean) {
+    // call API to save customer
+    console.log(model, isValid);
   }
 
 }
